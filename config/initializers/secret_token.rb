0a653b611e074912f4617667265ce7d662227afc
Davin Lagerroos <Davin@SSAI-IS-DL-6.local>
2012-04-26 15:55:40 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enoise::Application.config.secret_token = '50753a99e7850b144df56be7c1d2de56be66c262758f3df7060c9638770cf4a2fc93791f09ba8abe985274d95fca6d54b73241bbf32a026e2f742316ffb211c6'
