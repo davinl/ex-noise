eae32f6f42aeb3e2c604d51bf3fd38e6ce1f609c
Davin Lagerroos <Davin@SSAI-IS-DL-7.local>
2012-05-03 14:54:14 -0500

    name test passes. new build to heroku

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,7 @@ class Product < ActiveRecord::Base
   validates_numericality_of :price, 
                             :if => :price,
                             :greater_than => 0
+                            
+  validates_presence_of     :name
 
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -6,7 +6,7 @@ class ProductTest < ActiveSupport::TestCase
   # end
   
   def test_price_must_be_greater_than_0
-    p = Product.new(:price => -1)
+    p = Product.new(:name => "In On the Kill Taker", :price => -1)
     assert_equal false, p.valid?
     p.price = 0
     assert_equal false, p.valid?
@@ -17,5 +17,7 @@ class ProductTest < ActiveSupport::TestCase
   def test_name_cannot_be_blank
     p = Product.new(:price => 10)
     assert_equal false, p.valid?
+    p.name = "Strings of Life"
+    assert p.valid?
   end
 end
