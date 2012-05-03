95a7774d4fd1966e2272f92b5b0b4bd9ba7cdd90
Davin Lagerroos <Davin@SSAI-IS-DL-7.local>
2012-05-03 14:47:17 -0500

    another failing test build should fail

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -8,5 +8,14 @@ class ProductTest < ActiveSupport::TestCase
   def test_price_must_be_greater_than_0
     p = Product.new(:price => -1)
     assert_equal false, p.valid?
+    p.price = 0
+    assert_equal false, p.valid?
+    p.price = 0.01
+    assert p.valid?    
+  end
+  
+  def test_name_cannot_be_blank
+    p = Product.new(:price => 10)
+    assert_equal false, p.valid?
   end
 end
