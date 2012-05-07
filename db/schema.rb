c9d83b4da149414a3ed44fb670474d7b50cd042c
Davin Lagerroos <Davin@SSAI-IS-DL-7.local>
2012-05-07 14:47:22 -0500

    adding format to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120418170011) do
+ActiveRecord::Schema.define(:version => 20120507194439) do
 
   create_table "products", :force => true do |t|
     t.text     "name"
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "format"
   end
 
 end
