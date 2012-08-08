87f74883bc2feb0ecd4ec0051a6c024593311ade
Davin Lagerroos <dlagerroos@ssa-i.org>
2012-08-08 11:14:27 -0500

    move command to after heroku steps

--- db/populate/format.rb
+++ db/populate/format.rb
@@ -1,3 +1,4 @@
 Format.create_or_update(id: 1, name: 'Long Playing Record', abbreviation: 'LP')
 Format.create_or_update(id: 2, name: 'Compact Disc', abbreviation: 'CD')
 Format.create_or_update(id: 3, name: 'Seven Inch Extended Play Record', abbreviation: '7"')
+Format.create_or_update(id: 4, name: 'Cassette', abbreviation: 'CASS')
